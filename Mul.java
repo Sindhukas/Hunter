a0d1dcaf758d9bf0cb83eef4a82dbc99b74a0ce4
Sindhukas <ksindhukas@gmail.com>
2017-06-23 11:31:57 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,10 @@
+package Hunter;
+
+public class Mul {
+	public static void main(String[] args){
+		String s1=new String("5");
+		String s2=new String("6");
+		int res=Integer.parseInt(s1)*Integer.parseInt(s2);
+		System.out.println("The multiplication s:"+res);
+	}
+}
